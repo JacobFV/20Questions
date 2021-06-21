5c166150ea7491c9f40a1d4f68b97db4b4619774
Jacob Valdez <jacobfv123@gmail.com>
2021-06-21 14:13:52 -0500

    Dev

--- app/src/main/java/com/example/a20questions/QuestionarreActivity.kt
+++ app/src/main/java/com/example/a20questions/QuestionarreActivity.kt
@@ -8,4 +8,12 @@ class QuestionarreActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_questionarre)
     }
+
+    fun addQuestion(question: String) {
+        // TODO: Manish can you please add the question to the recycler view
+    }
+
+    fun displayGuess(guess: String) {
+        // TODO: Manish can you please add the guess to the recycler view
+    }
 }
