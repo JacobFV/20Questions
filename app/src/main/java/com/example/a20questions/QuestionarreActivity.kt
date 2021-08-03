258350a7b355bd4144d52b808eeff5cf891466c6
Jacob Valdez <jacobfv123@gmail.com>
2021-08-03 11:13:27 -0500

    message for Manish

--- app/src/main/java/com/example/a20questions/QuestionarreActivity.kt
+++ app/src/main/java/com/example/a20questions/QuestionarreActivity.kt
@@ -9,11 +9,39 @@ class QuestionarreActivity : AppCompatActivity() {
         setContentView(R.layout.activity_questionarre)
     }
 
-    fun addQuestion(question: String) {
-        // TODO: Manish can you please add the question to the recycler view
+    private fun answer(answer: Answer) {
+        // this is where all the game logic happens
     }
 
-    fun displayGuess(guess: String) {
-        // TODO: Manish can you please add the guess to the recycler view
+    private fun addTextElement(question: String) {
+        // This function is called by `answer`
+        // TODO: Manish can you please add a cardview to the recycler view
+        //       that displays a piece of text?
+    }
+
+    private fun addYesMaybeNoInput(question: String) {
+        // This function is called by `answer`
+        // TODO: Manish can you please add a cardview to the recycler view
+        //       that displays three buttons: "yes", "not sure", and "no" ?
+    }
+
+    private fun addYesNoInput(question: String) {
+        // This function is called by `answer`
+        // TODO: Manish can you please add a cardview to the recycler view
+        //       that displays three buttons: "yes" and "no" ?
+    }
+
+    fun answerYesPressed() {
+
+    }
+
+    fun answerNotSurePressed() {
+
+    }
+
+    fun answerNoPressed() {
+
     }
 }
+
+enum class Answer { YES, NOTSURE, NO }
